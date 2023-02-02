36bb9da75c4ee5edb893e145bda75d433da49dd7
RaymondOchoa <ochoaraymondb@yahoo.com>
2023-02-02 14:55:19 -0800

    completed nav show/index pending tests

--- src/mockCats.js
+++ src/mockCats.js
@@ -4,24 +4,21 @@ const cats = [
       name: "Mittens",
       age: 5,
       enjoys: "sunshine and warm spots",
-      image:
-        "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      image: "https://images.unsplash.com/photo-1517213849290-bbbfffdc6da3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
     },
     {
       id: 2,
       name: "Raisins",
       age: 6,
       enjoys: "being queen of the dogs",
-      image:
-        "https://images.unsplash.com/photo-1533743983669-94fa5c4338ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1092&q=80"
+      image: "https://images.unsplash.com/photo-1501820488136-72669149e0d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
     },
     {
       id: 3,
       name: "Toast",
       age: 3,
       enjoys: "getting all the attention",
-      image:
-        "https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      image: "https://images.unsplash.com/photo-1651840739969-e6765135619e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
     }
   ]
   export default cats
